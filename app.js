b329617edebbf5ce4a8a6ad92eb5fc3871d73afe
Srihari S <srihari.sundaramurugan@gmail.com>
2021-05-07 17:08:51 +0530

    Boom

--- app.js
+++ app.js
@@ -81,6 +81,6 @@ function CreateObject(data,i){
   this.ageLimit = data.sessions[i].min_age_limit;
 };
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
